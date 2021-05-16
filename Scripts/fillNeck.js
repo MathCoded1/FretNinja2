922db3f56659b2ce61b310e61420d29ccf1e4189
jonathan <jonathan@newmexicomathematical.org>
2021-05-16 10:33:39 -0600

    added change function and made tuning an array

--- Scripts/fillNeck.js
+++ Scripts/fillNeck.js
@@ -1,22 +1,5 @@
-first =  10
-second = 3
-third =  8
-fourth = 1
-fifth =  5
-sixth =  10
 
-var zero
-var one 
-var two 
-var three
-var four
-var five
-var six
-var seven
-var eight
-var nine
-var ten
-var eleven
+tuning = ["E", "A", "D", "G", "B", "E"]
 
 var fretNums ="     0        1        2        3        4        5        6        7        8        9        10       11       12       13       14       15       16       17       18       19       20       21"
 var fretLines="------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
@@ -27,22 +10,49 @@ return "||   "+a[0]+"  ||   "+a[1]+"  ||   "+a[2]+"  ||   "+a[3]+"  ||   "+a[5]+
 };
 console.log(fretNums)
 console.log(fretLines)
-console.log(fillstr(sixth))
-console.log(fillstr(fifth))
-console.log(fillstr(fourth))
-console.log(fillstr(third))
-console.log(fillstr(second))
-console.log(fillstr(first))
+console.log(fillstr(change(tuning[5])))
+console.log(fillstr(change(tuning[4])))
+console.log(fillstr(change(turning[3])))
+console.log(fillstr(change(tuning[2])))
+console.log(fillstr(change(tuning[1])))
+console.log(fillstr(change(tuning[0])))
 console.log(fretLines)
 console.log(fretNums)
 
 
+function change(note){
+    switch(note){
+    case "G":
+    return 1 
+    case "G#":
+    return 2
+    case "A":
+    return 3
+    case "A#":
+    return 4
+    case "B":
+    return 5
+    case "C":
+    return 6
+    case "C#":
+    return 7
+    case "D":
+    return 8
+    case "D#":
+    return 9
+    case "E":
+    return 10
+    case "F":
+    return 11
+    case "F#":
+    return 12
+    }
+    };
 
 
 
 
-
-function change(position){
+function changeBack(position){
 switch(position){
 case 1:
 return "G " 
@@ -73,18 +83,18 @@ return "F#"
 
 function fillarr(zeroth){
 var a = new Array(12)
-a[0] = change(zeroth)
-a[1] = change(add(zeroth,1))
-a[2] = change(add(zeroth,2))
-a[3] = change(add(zeroth,3))
-a[4] = change(add(zeroth,4))
-a[5] = change(add(zeroth,5))
-a[6] = change(add(zeroth,6))
-a[7] = change(add(zeroth,7))
-a[8] = change(add(zeroth,8))
-a[9] = change(add(zeroth,9))
-a[10] = change(add(zeroth,10))
-a[11] = change(add(zeroth,11))
+a[0] = changeBack(zeroth)
+a[1] = changeBack(add(zeroth,1))
+a[2] = changeBack(add(zeroth,2))
+a[3] = changeBack(add(zeroth,3))
+a[4] = changeBack(add(zeroth,4))
+a[5] = changeBack(add(zeroth,5))
+a[6] = changeBack(add(zeroth,6))
+a[7] = changeBack(add(zeroth,7))
+a[8] = changeBack(add(zeroth,8))
+a[9] = changeBack(add(zeroth,9))
+a[10] = changeBack(add(zeroth,10))
+a[11] = changeBack(add(zeroth,11))
 return a
 };
 
