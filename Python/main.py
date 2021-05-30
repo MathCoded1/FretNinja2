f42dde31ed7ea2a84da8a910a76b14ee822c6498
Jonathan <jonathan@localhost.localdomain>
2021-05-29 18:50:54 -0600

    added python project and distributed code

---
+++ Python/main.py
@@ -0,0 +1,618 @@
+import kivy.graphics
+from kivy.app import App
+from kivy.graphics import Color, Rectangle
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class GuitarTheory(Widget):
+    pass
+
+class Fret0String1(Widget):
+    pass
+
+class Fret0String2(Widget):
+    pass
+
+class Fret0String3(Widget):
+    pass
+
+class Fret0String4(Widget):
+    pass
+
+class Fret0String5(Widget):
+    pass
+
+class Fret0String6(Widget):
+    pass
+
+class Fret1String1(Widget):
+    pass
+
+class Fret1String2(Widget):
+    pass
+
+class Fret1String3(Widget):
+    pass
+
+class Fret1String4(Widget):
+    pass
+
+class Fret1String5(Widget):
+    pass
+
+class Fret1String6(Widget):
+    pass
+
+class Fret2String1(Widget):
+    pass
+
+class Fret2String2(Widget):
+    pass
+
+class Fret2String3(Widget):
+    pass
+
+class Fret2String4(Widget):
+    pass
+
+class Fret2String5(Widget):
+    pass
+
+class Fret2String6(Widget):
+    pass
+
+class Fret3String1(Widget):
+    pass
+
+class Fret3String2(Widget):
+    pass
+
+class Fret3String3(Widget):
+    pass
+
+class Fret3String4(Widget):
+    pass
+
+class Fret3String5(Widget):
+    pass
+
+class Fret3String6(Widget):
+    pass
+
+class Fret4String1(Widget):
+    pass
+
+class Fret4String2(Widget):
+    pass
+
+class Fret4String3(Widget):
+    pass
+
+class Fret4String4(Widget):
+    pass
+
+class Fret4String5(Widget):
+    pass
+
+class Fret4String6(Widget):
+    pass
+
+class Fret5String1(Widget):
+    pass
+
+class Fret5String2(Widget):
+    pass
+
+class Fret5String3(Widget):
+    pass
+
+class Fret5String4(Widget):
+    pass
+
+class Fret5String5(Widget):
+    pass
+
+class Fret5String6(Widget):
+    pass
+
+class Fret6String1(Widget):
+    pass
+
+class Fret6String2(Widget):
+    pass
+
+class Fret6String3(Widget):
+    pass
+
+class Fret6String4(Widget):
+    pass
+
+class Fret6String5(Widget):
+    pass
+
+class Fret6String6(Widget):
+    pass
+
+class Fret7String1(Widget):
+    pass
+
+class Fret7String2(Widget):
+    pass
+
+class Fret7String3(Widget):
+    pass
+
+class Fret7String4(Widget):
+    pass
+
+class Fret7String5(Widget):
+    pass
+
+class Fret7String6(Widget):
+    pass
+
+class Fret8String1(Widget):
+    pass
+
+class Fret8String2(Widget):
+    pass
+
+class Fret8String3(Widget):
+    pass
+
+class Fret8String4(Widget):
+    pass
+
+class Fret8String5(Widget):
+    pass
+
+class Fret8String6(Widget):
+    pass
+
+class Fret9String1(Widget):
+    pass
+
+class Fret9String2(Widget):
+    pass
+
+class Fret9String3(Widget):
+    pass
+
+class Fret9String4(Widget):
+    pass
+
+class Fret9String5(Widget):
+    pass
+
+class Fret9String6(Widget):
+    pass
+
+class Fret10String1(Widget):
+    pass
+
+class Fret10String2(Widget):
+    pass
+
+class Fret10String3(Widget):
+    pass
+
+class Fret10String4(Widget):
+    pass
+
+class Fret10String5(Widget):
+    pass
+
+class Fret10String6(Widget):
+    pass
+
+class Fret11String1(Widget):
+    pass
+
+class Fret11String2(Widget):
+    pass
+
+class Fret11String3(Widget):
+    pass
+
+class Fret11String4(Widget):
+    pass
+
+class Fret11String5(Widget):
+    pass
+
+class Fret11String6(Widget):
+    pass
+
+class Fret12String1(Widget):
+    pass
+
+class Fret12String2(Widget):
+    pass
+
+class Fret12String3(Widget):
+    pass
+
+class Fret12String4(Widget):
+    pass
+
+class Fret12String5(Widget):
+    pass
+
+class Fret12String6(Widget):
+    pass
+
+class Fret13String1(Widget):
+    pass
+
+class Fret13String2(Widget):
+    pass
+
+class Fret13String3(Widget):
+    pass
+
+class Fret13String4(Widget):
+    pass
+
+class Fret13String5(Widget):
+    pass
+
+class Fret13String6(Widget):
+    pass
+
+class Fret14String1(Widget):
+    pass
+
+class Fret14String2(Widget):
+    pass
+
+class Fret14String3(Widget):
+    pass
+
+class Fret14String4(Widget):
+    pass
+
+class Fret14String5(Widget):
+    pass
+
+class Fret14String6(Widget):
+    pass
+
+class Fret15String1(Widget):
+    pass
+
+class Fret15String2(Widget):
+    pass
+
+class Fret15String3(Widget):
+    pass
+
+class Fret15String4(Widget):
+    pass
+
+class Fret15String5(Widget):
+    pass
+
+class Fret15String6(Widget):
+    pass
+
+class Fret16String1(Widget):
+    pass
+
+class Fret16String2(Widget):
+    pass
+
+class Fret16String3(Widget):
+    pass
+
+class Fret16String4(Widget):
+    pass
+
+class Fret16String5(Widget):
+    pass
+
+class Fret16String6(Widget):
+    pass
+
+class Fret17String1(Widget):
+    pass
+
+class Fret17String2(Widget):
+    pass
+
+class Fret17String3(Widget):
+    pass
+
+class Fret17String4(Widget):
+    pass
+
+class Fret17String5(Widget):
+    pass
+
+class Fret17String6(Widget):
+    pass
+
+class Fret18String1(Widget):
+    pass
+
+class Fret18String2(Widget):
+    pass
+
+class Fret18String3(Widget):
+    pass
+
+class Fret18String4(Widget):
+    pass
+
+class Fret18String5(Widget):
+    pass
+
+class Fret18String6(Widget):
+    pass
+
+class Fret19String1(Widget):
+    pass
+
+class Fret19String2(Widget):
+    pass
+
+class Fret19String3(Widget):
+    pass
+
+class Fret19String4(Widget):
+    pass
+
+class Fret19String5(Widget):
+    pass
+
+class Fret19String6(Widget):
+    pass
+
+class Fret20String1(Widget):
+    pass
+
+class Fret20String2(Widget):
+    pass
+
+class Fret20String3(Widget):
+    pass
+
+class Fret20String4(Widget):
+    pass
+
+class Fret20String5(Widget):
+    pass
+
+class Fret20String6(Widget):
+    pass
+
+class Fret21String1(Widget):
+    pass
+
+class Fret21String2(Widget):
+    pass
+
+class Fret21String3(Widget):
+    pass
+
+class Fret21String4(Widget):
+    pass
+
+class Fret21String5(Widget):
+    pass
+
+class Fret21String6(Widget):
+    pass
+
+class Fret22String1(Widget):
+    pass
+
+class Fret22String2(Widget):
+    pass
+
+class Fret22String3(Widget):
+    pass
+
+class Fret22String4(Widget):
+    pass
+
+class Fret22String5(Widget):
+    pass
+
+class Fret22String6(Widget):
+    pass
+
+class Fret23String1(Widget):
+    pass
+
+class Fret23String2(Widget):
+    pass
+
+class Fret23String3(Widget):
+    pass
+
+class Fret23String4(Widget):
+    pass
+
+class Fret23String5(Widget):
+    pass
+
+class Fret23String6(Widget):
+    pass
+
+class Fret24String1(Widget):
+    pass
+
+class Fret24String2(Widget):
+    pass
+
+class Fret24String3(Widget):
+    pass
+
+class Fret24String4(Widget):
+    pass
+
+class Fret24String5(Widget):
+    pass
+
+class Fret24String6(Widget):
+    pass
+
+class GuitarApp(App):
+    def build(self):
+        root = GridLayout(cols = 21, rows = 6)
+        root.add_widget(Fret0String1)
+        root.add_widget(Fret1String1)
+        root.add_widget(Fret2String1)
+        root.add_widget(Fret3String1)
+        root.add_widget(Fret4String1)
+        root.add_widget(Fret5String1)
+        root.add_widget(Fret6String1)
+        root.add_widget(Fret7String1)
+        root.add_widget(Fret8String1)
+        root.add_widget(Fret9String1)
+        root.add_widget(Fret10String1)
+        root.add_widget(Fret11String1)
+        root.add_widget(Fret12String1)
+        root.add_widget(Fret13String1)
+        root.add_widget(Fret14String1)
+        root.add_widget(Fret15String1)
+        root.add_widget(Fret16String1)
+        root.add_widget(Fret17String1)
+        root.add_widget(Fret18String1)
+        root.add_widget(Fret19String1)
+        root.add_widget(Fret20String1)
+        root.add_widget(Fret21String1)
+        root.add_widget(Fret22String1)
+        root.add_widget(Fret23String1)
+        root.add_widget(Fret24String1)
+        root.add_widget(Fret0String2)
+        root.add_widget(Fret1String2)
+        root.add_widget(Fret2String2)
+        root.add_widget(Fret3String2)
+        root.add_widget(Fret4String2)
+        root.add_widget(Fret5String2)
+        root.add_widget(Fret6String2)
+        root.add_widget(Fret7String2)
+        root.add_widget(Fret8String2)
+        root.add_widget(Fret9String2)
+        root.add_widget(Fret10String2)
+        root.add_widget(Fret11String2)
+        root.add_widget(Fret12String2)
+        root.add_widget(Fret13String2)
+        root.add_widget(Fret14String2)
+        root.add_widget(Fret15String2)
+        root.add_widget(Fret16String2)
+        root.add_widget(Fret17String2)
+        root.add_widget(Fret18String2)
+        root.add_widget(Fret19String2)
+        root.add_widget(Fret20String2)
+        root.add_widget(Fret21String2)
+        root.add_widget(Fret22String2)
+        root.add_widget(Fret23String2)
+        root.add_widget(Fret24String2)
+        root.add_widget(Fret0String3)
+        root.add_widget(Fret1String3)
+        root.add_widget(Fret2String3)
+        root.add_widget(Fret3String3)
+        root.add_widget(Fret4String3)
+        root.add_widget(Fret5String3)
+        root.add_widget(Fret6String3)
+        root.add_widget(Fret7String3)
+        root.add_widget(Fret8String3)
+        root.add_widget(Fret9String3)
+        root.add_widget(Fret10String3)
+        root.add_widget(Fret11String3)
+        root.add_widget(Fret12String3)
+        root.add_widget(Fret13String3)
+        root.add_widget(Fret14String3)
+        root.add_widget(Fret15String3)
+        root.add_widget(Fret16String3)
+        root.add_widget(Fret17String3)
+        root.add_widget(Fret18String3)
+        root.add_widget(Fret19String3)
+        root.add_widget(Fret20String3)
+        root.add_widget(Fret21String3)
+        root.add_widget(Fret22String3)
+        root.add_widget(Fret23String3)
+        root.add_widget(Fret24String3)
+        root.add_widget(Fret0String4)
+        root.add_widget(Fret1String4)
+        root.add_widget(Fret2String4)
+        root.add_widget(Fret3String4)
+        root.add_widget(Fret4String4)
+        root.add_widget(Fret5String4)
+        root.add_widget(Fret6String4)
+        root.add_widget(Fret7String4)
+        root.add_widget(Fret8String4)
+        root.add_widget(Fret9String4)
+        root.add_widget(Fret10String4)
+        root.add_widget(Fret11String4)
+        root.add_widget(Fret12String4)
+        root.add_widget(Fret13String4)
+        root.add_widget(Fret14String4)
+        root.add_widget(Fret15String4)
+        root.add_widget(Fret16String4)
+        root.add_widget(Fret17String4)
+        root.add_widget(Fret18String4)
+        root.add_widget(Fret19String4)
+        root.add_widget(Fret20String4)
+        root.add_widget(Fret21String4)
+        root.add_widget(Fret22String4)
+        root.add_widget(Fret23String4)
+        root.add_widget(Fret24String4)
+        root.add_widget(Fret0String5)
+        root.add_widget(Fret1String5)
+        root.add_widget(Fret2String5)
+        root.add_widget(Fret3String5)
+        root.add_widget(Fret4String5)
+        root.add_widget(Fret5String5)
+        root.add_widget(Fret6String5)
+        root.add_widget(Fret7String5)
+        root.add_widget(Fret8String5)
+        root.add_widget(Fret9String5)
+        root.add_widget(Fret10String5)
+        root.add_widget(Fret11String5)
+        root.add_widget(Fret12String5)
+        root.add_widget(Fret13String5)
+        root.add_widget(Fret14String5)
+        root.add_widget(Fret15String5)
+        root.add_widget(Fret16String5)
+        root.add_widget(Fret17String5)
+        root.add_widget(Fret18String5)
+        root.add_widget(Fret19String5)
+        root.add_widget(Fret20String5)
+        root.add_widget(Fret21String5)
+        root.add_widget(Fret22String5)
+        root.add_widget(Fret23String5)
+        root.add_widget(Fret24String5)
+        root.add_widget(Fret0String6)
+        root.add_widget(Fret1String6)
+        root.add_widget(Fret2String6)
+        root.add_widget(Fret3String6)
+        root.add_widget(Fret4String6)
+        root.add_widget(Fret5String6)
+        root.add_widget(Fret6String6)
+        root.add_widget(Fret7String6)
+        root.add_widget(Fret8String6)
+        root.add_widget(Fret9String6)
+        root.add_widget(Fret10String6)
+        root.add_widget(Fret11String6)
+        root.add_widget(Fret12String6)
+        root.add_widget(Fret13String6)
+        root.add_widget(Fret14String6)
+        root.add_widget(Fret15String6)
+        root.add_widget(Fret16String6)
+        root.add_widget(Fret17String6)
+        root.add_widget(Fret18String6)
+        root.add_widget(Fret19String6)
+        root.add_widget(Fret20String6)
+        root.add_widget(Fret21String6)
+        root.add_widget(Fret22String6)
+        root.add_widget(Fret23String6)
+        root.add_widget(Fret24String6)
+        return root
+
+
+if __name__ == '__main__':
+    GuitarApp().run()
