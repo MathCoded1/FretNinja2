1104efd8ca7ca301da322b0e0a21bce03c262943
Jonathan <jonathan@localhost.localdomain>
2021-05-30 01:49:45 -0600

    got fretboard looking like buttons

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,9 @@
 import kivy.graphics
 from kivy.app import App
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Rectangle, Canvas, Callback
+from kivy.properties import ListProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
@@ -9,458 +12,534 @@ import fillNeck
 class GuitarTheory(Widget):
     pass
 
-class Fret0String1(Widget):
-    pass
-
-class Fret0String2(Widget):
-    pass
-
-class Fret0String3(Widget):
-    pass
-
-class Fret0String4(Widget):
-    pass
-
-class Fret0String5(Widget):
-    pass
-
-class Fret0String6(Widget):
-    pass
-
-class Fret1String1(Widget):
-    pass
-
-class Fret1String2(Widget):
-    pass
-
-class Fret1String3(Widget):
-    pass
-
-class Fret1String4(Widget):
-    pass
-
-class Fret1String5(Widget):
-    pass
-
-class Fret1String6(Widget):
-    pass
-
-class Fret2String1(Widget):
-    pass
-
-class Fret2String2(Widget):
-    pass
-
-class Fret2String3(Widget):
-    pass
-
-class Fret2String4(Widget):
-    pass
-
-class Fret2String5(Widget):
-    pass
-
-class Fret2String6(Widget):
-    pass
-
-class Fret3String1(Widget):
-    pass
-
-
-class Fret3String2(Widget):
-    pass
-
-class Fret3String3(Widget):
-    pass
+class ColoredLabel(Label):
+    background_color = ListProperty((1, 1, 1, 1))
+
+class Fret:
+    pass
+
+class Fret0String1(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret0String1, self).__init__(**kwargs)
+        self.text = 'E'
+
+class Fret0String2(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret0String2, self).__init__(**kwargs)
+        self.text = 'A'
+
+class Fret0String3(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret0String3, self).__init__(**kwargs)
+        self.text = 'D'
+
+class Fret0String4(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret0String4, self).__init__(**kwargs)
+        self.text = 'G'
+
+class Fret0String5(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret0String5, self).__init__(**kwargs)
+        self.text = 'B'
+
+class Fret0String6(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret0String6, self).__init__(**kwargs)
+        self.text = 'E'
+
+class Fret1String1(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret1String1, self).__init__(**kwargs)
+        self.text = 'F'
+
+class Fret1String2(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret1String2, self).__init__(**kwargs)
+        self.text = 'A#'
+
+class Fret1String3(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret1String3, self).__init__(**kwargs)
+        self.text = 'D#'
+
+class Fret1String4(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret1String4, self).__init__(**kwargs)
+        self.text = 'G#'
+
+class Fret1String5(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret1String5, self).__init__(**kwargs)
+        self.text = 'C'
+
+class Fret1String6(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret1String6, self).__init__(**kwargs)
+        self.text = 'F'
+
+class Fret2String1(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret2String1, self).__init__(**kwargs)
+        self.text = 'F#'
+
+class Fret2String2(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret2String2, self).__init__(**kwargs)
+        self.text = 'B'
+
+class Fret2String3(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret2String3, self).__init__(**kwargs)
+        self.text = 'E'
+
+class Fret2String4(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret2String4, self).__init__(**kwargs)
+        self.text = 'A'
+
+class Fret2String5(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret2String5, self).__init__(**kwargs)
+        self.text = 'C#'
+
+class Fret2String6(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret2String6, self).__init__(**kwargs)
+        self.text = 'F#'
+
+class Fret3String1(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret3String1, self).__init__(**kwargs)
+        self.text = 'G'
+
+class Fret3String2(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret3String2, self).__init__(**kwargs)
+        self.text = 'C'
 
-class Fret3String4(Widget):
-    pass
+class Fret3String3(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret3String3, self).__init__(**kwargs)
+        self.text = 'F'
 
-class Fret3String5(Widget):
-    pass
+class Fret3String4(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret3String4, self).__init__(**kwargs)
+        self.text = 'A#'
 
-class Fret3String6(Widget):
-    pass
+class Fret3String5(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret3String5, self).__init__(**kwargs)
+        self.text = 'D'
 
-class Fret4String1(Widget):
-    pass
+class Fret3String6(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret3String6, self).__init__(**kwargs)
+        self.text = 'G'
 
-class Fret4String2(Widget):
-    pass
+class Fret4String1(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret4String1, self).__init__(**kwargs)
+        self.text = 'G#'
 
-class Fret4String3(Widget):
-    pass
+class Fret4String2(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret4String2, self).__init__(**kwargs)
+        self.text = 'C#'
 
-class Fret4String4(Widget):
-    pass
+class Fret4String3(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret4String3, self).__init__(**kwargs)
+        self.text = 'F#'
 
-class Fret4String5(Widget):
-    pass
+class Fret4String4(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret4String4, self).__init__(**kwargs)
+        self.text = 'B'
 
-class Fret4String6(Widget):
-    pass
+class Fret4String5(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret4String5, self).__init__(**kwargs)
+        self.text = 'D#'
 
-class Fret5String1(Widget):
-    pass
+class Fret4String6(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret4String6, self).__init__(**kwargs)
+        self.text = 'G#'
 
-class Fret5String2(Widget):
-    pass
+class Fret5String1(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret5String1, self).__init__(**kwargs)
+        self.text = 'A'
 
-class Fret5String3(Widget):
-    pass
+class Fret5String2(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret5String2, self).__init__(**kwargs)
+        self.text = 'D'
 
-class Fret5String4(Widget):
-    pass
+class Fret5String3(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret5String3, self).__init__(**kwargs)
+        self.text = 'G'
+class Fret5String4(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret5String4, self).__init__(**kwargs)
+        self.text = 'C'
 
-class Fret5String5(Widget):
-    pass
+class Fret5String5(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret5String5, self).__init__(**kwargs)
+        self.text = 'E'
 
-class Fret5String6(Widget):
-    pass
+class Fret5String6(Button, Fret):
+    def __init__(self, **kwargs):
+        super(Fret5String6, self).__init__(**kwargs)
+        self.text = 'A'
 
-class Fret6String1(Widget):
+class Fret6String1(Button, Fret):
     pass
 
-class Fret6String2(Widget):
+class Fret6String2(Button, Fret):
     pass
 
-class Fret6String3(Widget):
+class Fret6String3(Button, Fret):
     pass
 
-class Fret6String4(Widget):
+class Fret6String4(Button, Fret):
     pass
 
-class Fret6String5(Widget):
+class Fret6String5(Button, Fret):
     pass
 
-class Fret6String6(Widget):
+class Fret6String6(Button, Fret):
     pass
 
-class Fret7String1(Widget):
+class Fret7String1(Button, Fret):
     pass
 
-class Fret7String2(Widget):
+class Fret7String2(Button, Fret):
     pass
 
-class Fret7String3(Widget):
+class Fret7String3(Button, Fret):
     pass
 
-class Fret7String4(Widget):
+class Fret7String4(Button, Fret):
     pass
 
-class Fret7String5(Widget):
+class Fret7String5(Button, Fret):
     pass
 
-class Fret7String6(Widget):
+class Fret7String6(Button, Fret):
     pass
 
-class Fret8String1(Widget):
+class Fret8String1(Button, Fret):
     pass
 
-class Fret8String2(Widget):
+class Fret8String2(Button, Fret):
     pass
 
-class Fret8String3(Widget):
+class Fret8String3(Button, Fret):
     pass
 
-class Fret8String4(Widget):
+class Fret8String4(Button, Fret):
     pass
 
-class Fret8String5(Widget):
+class Fret8String5(Button, Fret):
     pass
 
-class Fret8String6(Widget):
+class Fret8String6(Button, Fret):
     pass
 
-class Fret9String1(Widget):
+class Fret9String1(Button, Fret):
     pass
 
-class Fret9String2(Widget):
+class Fret9String2(Button, Fret):
     pass
 
-class Fret9String3(Widget):
+class Fret9String3(Button, Fret):
     pass
 
-class Fret9String4(Widget):
+class Fret9String4(Button, Fret):
     pass
 
-class Fret9String5(Widget):
+class Fret9String5(Button, Fret):
     pass
 
-class Fret9String6(Widget):
+class Fret9String6(Button, Fret):
     pass
 
-class Fret10String1(Widget):
+class Fret10String1(Button, Fret):
     pass
 
-class Fret10String2(Widget):
+class Fret10String2(Button, Fret):
     pass
 
-class Fret10String3(Widget):
+class Fret10String3(Button, Fret):
     pass
 
-class Fret10String4(Widget):
+class Fret10String4(Button, Fret):
     pass
 
-class Fret10String5(Widget):
+class Fret10String5(Button, Fret):
     pass
 
-class Fret10String6(Widget):
+class Fret10String6(Button, Fret):
     pass
 
-class Fret11String1(Widget):
+class Fret11String1(Button, Fret):
     pass
 
-class Fret11String2(Widget):
+class Fret11String2(Button, Fret):
     pass
 
-class Fret11String3(Widget):
+class Fret11String3(Button, Fret):
     pass
 
-class Fret11String4(Widget):
+class Fret11String4(Button, Fret):
     pass
 
-class Fret11String5(Widget):
+class Fret11String5(Button, Fret):
     pass
 
-class Fret11String6(Widget):
+class Fret11String6(Button, Fret):
     pass
 
-class Fret12String1(Widget):
+class Fret12String1(Button, Fret):
     pass
 
-class Fret12String2(Widget):
+class Fret12String2(Button, Fret):
     pass
 
-class Fret12String3(Widget):
+class Fret12String3(Button, Fret):
     pass
 
-class Fret12String4(Widget):
+class Fret12String4(Button, Fret):
     pass
 
-class Fret12String5(Widget):
+class Fret12String5(Button, Fret):
     pass
 
-class Fret12String6(Widget):
+class Fret12String6(Button, Fret):
     pass
 
-class Fret13String1(Widget):
+class Fret13String1(Button, Fret):
     pass
 
-class Fret13String2(Widget):
+class Fret13String2(Button, Fret):
     pass
 
-class Fret13String3(Widget):
+class Fret13String3(Button, Fret):
     pass
 
-class Fret13String4(Widget):
+class Fret13String4(Button, Fret):
     pass
 
-class Fret13String5(Widget):
+class Fret13String5(Button, Fret):
     pass
 
-class Fret13String6(Widget):
+class Fret13String6(Button, Fret):
     pass
 
-class Fret14String1(Widget):
+class Fret14String1(Button, Fret):
     pass
 
-class Fret14String2(Widget):
+class Fret14String2(Button, Fret):
     pass
 
-class Fret14String3(Widget):
+class Fret14String3(Button, Fret):
     pass
 
-class Fret14String4(Widget):
+class Fret14String4(Button, Fret):
     pass
 
-class Fret14String5(Widget):
+class Fret14String5(Button, Fret):
     pass
 
-class Fret14String6(Widget):
+class Fret14String6(Button, Fret):
     pass
 
-class Fret15String1(Widget):
+class Fret15String1(Button, Fret):
     pass
 
-class Fret15String2(Widget):
+class Fret15String2(Button, Fret):
     pass
 
-class Fret15String3(Widget):
+class Fret15String3(Button, Fret):
     pass
 
-class Fret15String4(Widget):
+class Fret15String4(Button, Fret):
     pass
 
-class Fret15String5(Widget):
+class Fret15String5(Button, Fret):
     pass
 
-class Fret15String6(Widget):
+class Fret15String6(Button, Fret):
     pass
 
-class Fret16String1(Widget):
+class Fret16String1(Button, Fret):
     pass
 
-class Fret16String2(Widget):
+class Fret16String2(Button, Fret):
     pass
 
-class Fret16String3(Widget):
+class Fret16String3(Button, Fret):
     pass
 
-class Fret16String4(Widget):
+class Fret16String4(Button, Fret):
     pass
 
-class Fret16String5(Widget):
+class Fret16String5(Button, Fret):
     pass
 
-class Fret16String6(Widget):
+class Fret16String6(Button, Fret):
     pass
 
-class Fret17String1(Widget):
+class Fret17String1(Button, Fret):
     pass
 
-class Fret17String2(Widget):
+class Fret17String2(Button, Fret):
     pass
 
-class Fret17String3(Widget):
+class Fret17String3(Button, Fret):
     pass
 
-class Fret17String4(Widget):
+class Fret17String4(Button, Fret):
     pass
 
-class Fret17String5(Widget):
+class Fret17String5(Button, Fret):
     pass
 
-class Fret17String6(Widget):
+class Fret17String6(Button, Fret):
     pass
 
-class Fret18String1(Widget):
+class Fret18String1(Button, Fret):
     pass
 
-class Fret18String2(Widget):
+class Fret18String2(Button, Fret):
     pass
 
-class Fret18String3(Widget):
+class Fret18String3(Button, Fret):
     pass
 
-class Fret18String4(Widget):
+class Fret18String4(Button, Fret):
     pass
 
-class Fret18String5(Widget):
+class Fret18String5(Button, Fret):
     pass
 
-class Fret18String6(Widget):
+class Fret18String6(Button, Fret):
     pass
 
-class Fret19String1(Widget):
+class Fret19String1(Button, Fret):
     pass
 
-class Fret19String2(Widget):
+class Fret19String2(Button, Fret):
     pass
 
-class Fret19String3(Widget):
+class Fret19String3(Button, Fret):
     pass
 
-class Fret19String4(Widget):
+class Fret19String4(Button, Fret):
     pass
 
-class Fret19String5(Widget):
+class Fret19String5(Button, Fret):
     pass
 
-class Fret19String6(Widget):
+class Fret19String6(Button, Fret):
     pass
 
-class Fret20String1(Widget):
+class Fret20String1(Button, Fret):
     pass
 
-class Fret20String2(Widget):
+class Fret20String2(Button, Fret):
     pass
 
-class Fret20String3(Widget):
+class Fret20String3(Button, Fret):
     pass
 
-class Fret20String4(Widget):
+class Fret20String4(Button, Fret):
     pass
 
-class Fret20String5(Widget):
+class Fret20String5(Button, Fret):
     pass
 
-class Fret20String6(Widget):
+class Fret20String6(Button, Fret):
     pass
 
-class Fret21String1(Widget):
+class Fret21String1(Button, Fret):
     pass
 
-class Fret21String2(Widget):
+class Fret21String2(Button, Fret):
     pass
 
-class Fret21String3(Widget):
+class Fret21String3(Button, Fret):
     pass
 
-class Fret21String4(Widget):
+class Fret21String4(Button, Fret):
     pass
 
-class Fret21String5(Widget):
+class Fret21String5(Button, Fret):
     pass
 
-class Fret21String6(Widget):
+class Fret21String6(Button, Fret):
     pass
 
-class Fret22String1(Widget):
+class Fret22String1(Button, Fret):
     pass
 
-class Fret22String2(Widget):
+class Fret22String2(Button, Fret):
     pass
 
-class Fret22String3(Widget):
+class Fret22String3(Button, Fret):
     pass
 
-class Fret22String4(Widget):
+class Fret22String4(Button, Fret):
     pass
 
-class Fret22String5(Widget):
+class Fret22String5(Button, Fret):
     pass
 
-class Fret22String6(Widget):
+class Fret22String6(Button, Fret):
     pass
 
-class Fret23String1(Widget):
+class Fret23String1(Button, Fret):
     pass
 
-class Fret23String2(Widget):
+class Fret23String2(Button, Fret):
     pass
 
-class Fret23String3(Widget):
+class Fret23String3(Button, Fret):
     pass
 
-class Fret23String4(Widget):
+class Fret23String4(Button, Fret):
     pass
 
-class Fret23String5(Widget):
+class Fret23String5(Button, Fret):
     pass
 
-class Fret23String6(Widget):
+class Fret23String6(Button, Fret):
     pass
 
-class Fret24String1(Widget):
+class Fret24String1(Button, Fret):
     pass
 
-class Fret24String2(Widget):
+class Fret24String2(Button, Fret):
     pass
 
-class Fret24String3(Widget):
+class Fret24String3(Button, Fret):
     pass
 
-class Fret24String4(Widget):
+class Fret24String4(Button, Fret):
     pass
 
-class Fret24String5(Widget):
+class Fret24String5(Button, Fret):
     pass
 
-class Fret24String6(Widget):
+class Fret24String6(Button, Fret):
     pass
 
-class Fret0Label(Widget):
+class Fret0Label(Button, Fret):
     pass
 
 class Fret1Label(Widget):
@@ -548,6 +627,7 @@ class GuitarApp(App):
     def build(self):
         root = Grid()
         root.add_widget(Fret0String6())
+
         root.add_widget(Fret1String6(width=20))
         root.add_widget(Fret2String6(width=20))
         root.add_widget(Fret3String6(width=20))
